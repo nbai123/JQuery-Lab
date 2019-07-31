e01f58f76bc9e5f05fe1a797b3f7d92b3db0ecdb
Nick Bai <nick@nick.com>
2019-07-31 16:27:33 -0700

    Updates to index and css and add pseudo

---
+++ js/main.js
@@ -0,0 +1,3 @@
+//When page is open no input values show on screen
+//after inputting an item in empty field click button and show item on table above
+//when item gets added to table include button with the add that can remove the input after it is on the table
